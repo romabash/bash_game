1b2b4d93b5662ef24286ecaa76793f50f79c959a
Roma <romanofkin@gmail.com>
2017-03-05 12:56:13 -0800

    Added player and player's movements

--- game.sh
+++ game.sh
@@ -10,14 +10,46 @@ rows=$(tput lines)
 middle_row=$((rows/2))
 middle_col=$(( (cols/2)-(${#x}/2) ))
 
-for ((i=1; i<=5; i++)); do
+
+#Player Green Bg with Green text
+y="\033[0;102m\033[1;92m======\e[0m"
+y_pos=$middle_col
+left='\[D'
+right='\[C'
+SUCCESS=0
+
+for ((x_pos=1; x_pos<=10; x_pos++)); do
   tput clear
 
-  tput cup $((10+$i)) $middle_col
+  tput cup $x_pos $middle_col
   echo -e ${x}
+ 
+
+  tput cup $rows $y_pos
+  echo -e ${y}
+  
+  read -rsn3 input
+  echo -n "$input" | grep "$left"
+  if [ "$?" -eq $SUCCESS ]; then
+    let y_pos-=10
+  fi
+  echo -n "$input" | grep "$right"
+  if [ "$?" -eq $SUCCESS ]; then
+    let y_pos+=10
+  fi
+
+#if ["$input" = "a"]; then
+#  $((++y_pos))
+#  echo $input
+#elif ["$input" = "s"]; then
+#  $((--y_pos))  
+#fi
+
   sleep 0.5
 done
 
+
 tput sgr0
 tput cup $(tput lines) 0
+
 
