87ad915d1be0cdf08fd55af52435e92d4c82a744
Roma <romanofkin@gmail.com>
2017-03-08 20:58:03 -0800

    Function to check if element is in array.  Able to generate unique random numbers

--- game.sh
+++ game.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-#num=$(( $RANDOM % 10 ))
-#echo $num
+#Function to check in element is in array
+inArray(){
+  local e #local varibale e 
+  local arr="${@:2}" #Combines 2nd arg (expanded array) into one varibale  
+     
+  for e in $arr; do 
+    if [[ "$e" == "$1" ]]; then 
+      return 0 #Return 0 (True) 
+    fi 
+  done
+  return 1 #Return 1 (False) otherwise
+}
 
 number=()
 
-#len=${#number[@]}
+len=${#number[@]}
 
 while (( ${#number[@]} < 4 )); do
   num=$(( $RANDOM % 10 ))
-  echo $num
-  for i in "${number[@]}"; do
-    if [ "$i" -eq "$num" ]; then
-      result="present"
-    fi 
-  done 
-  if [ "$result" != "present" ]; then
-    number+=($num)
+
+  inArray $num ${number[@]}
+  contains=$?
+  
+  if (( $contains == 1 )); then
+    number+=($num)  
   fi
 done
 
