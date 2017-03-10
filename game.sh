21484e40c199c23b2af77ee1b0a673667168bc8a
Roma <romanofkin@gmail.com>
2017-03-09 23:51:47 -0800

    Verifying user input for correct number.  Started checking for bulls and cows

--- game.sh
+++ game.sh
@@ -17,6 +17,7 @@ inArray(){
 }
 
 #-----------------------------------------------------------------------
+#Generating Random number
 
 number=() 
 level=4 #How many numbers to play 
@@ -34,35 +35,54 @@ done
 
 echo ${number[*]}
 
-
 #-----------------------------------------------------------------------
-read -p "Enter a unique $level digit number " input 
-guess=($(echo "$input" | grep -o .)) #Outer () to make array
 
-echo ${guess[@]}
+bulls=0
+cows=0
+#Input Validation
 
-all_matches=0 #Find all matches first
-for i in ${guess[@]}; do 
-  inArray $i ${number[@]}
-  match=$?
+while (( $bulls != $level )); do
+  read -p "Enter a unique $level digit number " input 
+  guess=($(echo "$input" | grep -o .)) #Outer () to make array
   
-  if (( $match == 0)); then #If number is in both arrays
-    let all_matches+=1
+  if (( ${#guess[@]} != $level )); then
+    read -p "Enter a unique $level digit number " input 
+    guess=($(echo "$input" | grep -o .)) #Outer () to make array
   fi
-done
-
-bulls=0 #Find number of bulls by looking at numbers in the same index 
-for x in ${!guess[@]}; do
-  if (( ${guess[$x]} == ${number[$x]} )); then
-    let bulls+=1
+  
+  check=($(echo "${guess[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+  
+  if (( ${#guess[@]} != ${#check[@]} )); then
+    read -p "Enter a UNIQUE $level digit number " input 
+    guess=($(echo "$input" | grep -o .)) #Outer () to make array
   fi
-done 
+  
+  echo ${guess[@]}
+ 
+  #Checking and comparing
+  all_matches=0 #Find all matches first
+  for i in ${guess[@]}; do 
+    inArray $i ${number[@]}
+    match=$?
+    
+    if (( $match == 0)); then #If number is in both arrays
+      let all_matches+=1
+    fi
+  done
+  
+  #Find number of bulls by looking at numbers in the same index 
+  for x in ${!guess[@]}; do
+    if (( ${guess[$x]} == ${number[$x]} )); then
+      let bulls+=1
+    fi
+  done 
+  
+  cows=$(( all_matches - bulls )) #Find cows by subtracting
+done  
+  echo "Total matches is $all_matches"
+  echo "Bulls: $bulls"
+  echo "Cows $cows"
 
-cows=$(( all_matches - bulls )) #Find cows by subtracting
-
-echo "Total matches is $all_matches"
-echo "Bulls: $bulls"
-echo "Cows $cows"
 
 
 
