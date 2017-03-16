908773759907375edcbc7b9bf0eb0eba2b1594dd
Roma <romanofkin@gmail.com>
2017-03-15 21:37:07 -0700

    Working game with checks

--- game.sh
+++ game.sh
@@ -6,57 +6,74 @@ source ./randomNumber.sh
 #Generating Random number
 
 level=4
-number=($(randomNumber $level)) 
+number=($(randomNumber $level)) #set to array 
 echo ${number[*]}
 
 #-----------------------------------------------------------------------
 
-bulls=0
-cows=0
 #Input Validation
+#
+#read -p "Enter a unique $level digit number " input 
+#guess=($(echo "$input" | grep -o .)) #Outer () to make array
+#  
+#if (( ${#guess[@]} != $level )); then
+#  read -p "Enter a unique $level digit number " input 
+#  guess=($(echo "$input" | grep -o .)) #Outer () to make array
+#fi
+#  
+#check=($(echo "${guess[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#  
+#if (( ${#guess[@]} != ${#check[@]} )); then
+#  read -p "Enter a UNIQUE $level digit number " input 
+#  guess=($(echo "$input" | grep -o .)) #Outer () to make array
+#fi
+#  
+#echo ${guess[@]}
+ 
+#-----------------------------------------------------------------------
 
-while (( $bulls != $level )); do
-  read -p "Enter a unique $level digit number " input 
+#Checking and comparing
+bulls=0
+until (($bulls >= $level)); do 
+   
+  read -p "Enter your guess " input
   guess=($(echo "$input" | grep -o .)) #Outer () to make array
+  check=($(echo "${guess[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')) #Remove duplicates and sets new array
+
+  bulls=0
+  cows=0
   
   if (( ${#guess[@]} != $level )); then
-    read -p "Enter a unique $level digit number " input 
-    guess=($(echo "$input" | grep -o .)) #Outer () to make array
-  fi
-  
-  check=($(echo "${guess[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-  
-  if (( ${#guess[@]} != ${#check[@]} )); then
-    read -p "Enter a UNIQUE $level digit number " input 
-    guess=($(echo "$input" | grep -o .)) #Outer () to make array
-  fi
-  
-  echo ${guess[@]}
- 
-  #Checking and comparing
-  all_matches=0 #Find all matches first
-  for i in ${guess[@]}; do 
-    inArray $i ${number[@]}
-    match=$?
+    echo "Make sure to use $level numbers" 
+  elif (( ${#guess[@]} != ${#check[@]} )); then
+    echo "Make sure to not duplicate numbers" 
+  else
     
-    if (( $match == 0)); then #If number is in both arrays
-      let all_matches+=1
-    fi
-  done
+    all_matches=0 #Find all matches first
+    for i in ${guess[@]}; do 
+      inArray $i ${number[@]}
+      match=$?
+        
+      if (( $match == 0)); then #If number is in both arrays
+        let all_matches+=1
+      fi
+    done 
+      
+    #Find number of bulls by looking at numbers in the same index 
+    for x in ${!guess[@]}; do
+      if (( ${guess[$x]} == ${number[$x]} )); then
+        let bulls+=1
+      fi
+    done 
+      
+    cows=$(( all_matches - bulls )) #Find cows by subtracting
+    
+    echo "Total matches is $all_matches"
+    echo "Bulls: $bulls"
+    echo "Cows $cows"
+  fi
   
-  #Find number of bulls by looking at numbers in the same index 
-  for x in ${!guess[@]}; do
-    if (( ${guess[$x]} == ${number[$x]} )); then
-      let bulls+=1
-    fi
-  done 
-  
-  cows=$(( all_matches - bulls )) #Find cows by subtracting
-done  
-  echo "Total matches is $all_matches"
-  echo "Bulls: $bulls"
-  echo "Cows $cows"
-
+done #Ends untill loop
 
 
 
